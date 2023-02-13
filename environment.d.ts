8057d104f21ebb8adf1fdb9f8be82640efc40b7c
Awokens <35590883+Awokens@users.noreply.github.com>
2023-02-13 13:39:19 -0800

    Update environment.d.ts

--- environment.d.ts
+++ environment.d.ts
@@ -2,6 +2,8 @@ declare global {
     namespace NodeJS {
         interface ProcessEnv {
             botToken: string;
+            guildId: string;
+            clientId: string;
         }
     }
 }
